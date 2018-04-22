aa9bc95feb724e8a7c39055974c8a24be849ed71
Jiří Šebele <sindriava@gmail.com>
2018-04-22 15:52:52 +0200

    Empty main.cpp in the library code

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,5 @@
 #include <Mcu.hpp>
 
 int main() {
-    Mcu mcu;
-
-    mcu.load_program({ 0x02, 0x00, 0xFF, 0xFF, 0x02, 0x01, 0x00, 0x01, 0x10, 0x01 });
-    mcu.step();
-    mcu.step();
-    mcu.step();
-
-    fmt::print("R0: 0x{:x}\nR1: 0x{:x}\nCF: {}", mcu.registers[0], mcu.registers[1], mcu.flags.carry);
-
     return 0;
 }
