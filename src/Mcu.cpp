222af51cf312d445fac6c349779ef4c227fe7852
Jiří Šebele <sindriava@gmail.com>
2018-04-24 13:06:53 +0200

    Reimplement high / low byte macros as functions

--- src/Mcu.cpp
+++ src/Mcu.cpp
@@ -10,8 +10,13 @@
 
 #include <opcodes.hpp>
 
-#define HIGH_BYTE(X) static_cast<uint8_t>(((X) & 0xFF00u) >> 8u)
-#define LOW_BYTE(X) static_cast<uint8_t>(((X) & 0x00FFu) >> 0u)
+constexpr inline u8 high_byte(u16 x) {
+    return static_cast<u8>((x & 0xFF00u) >> 8u);
+}
+
+constexpr inline u8 low_byte(u16 x) {
+    return static_cast<u8>((x & 0x00FFu) >> 0u);
+}
 
 constexpr inline u8 high_nibble(u8 x) {
     return static_cast<u8>((x & 0xF0u) >> 4u);
