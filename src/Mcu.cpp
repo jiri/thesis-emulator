5fdd53db4826e95885f69e5ee24f9c4b07948e8d
Jiří Šebele <sindriava@gmail.com>
2018-04-28 22:03:33 +0200

    Split byte operations into util.hpp

--- src/Mcu.cpp
+++ src/Mcu.cpp
@@ -7,22 +7,7 @@
 
 #include <opcodes.hpp>
 #include <interrupts.hpp>
-
-constexpr inline u8 high_byte(u16 x) {
-    return static_cast<u8>((x & 0xFF00u) >> 8u);
-}
-
-constexpr inline u8 low_byte(u16 x) {
-    return static_cast<u8>((x & 0x00FFu) >> 0u);
-}
-
-constexpr inline u8 high_nibble(u8 x) {
-    return static_cast<u8>((x & 0xF0u) >> 4u);
-}
-
-constexpr inline u8 low_nibble(u8 x) {
-    return static_cast<u8>((x & 0x0Fu) >> 0u);
-}
+#include <util.hpp>
 
 void Mcu::load_program(const std::vector<u8>& binary) {
     assert(this->program.size() >= binary.size());
